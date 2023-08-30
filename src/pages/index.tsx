db915df92dd31c1aaab34ed0909c3e6c5dd91b71
Gabriela Porto <gabriela.bairros.porto96@gmail.com>
2023-08-30 07:06:02 -0300

    feature: table with random data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import { Button, Table } from 'antd'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Button } from 'antd'
 
-const inter = Inter({ subsets: ['latin'] })
+import styles from '@/styles/Home.module.css';
+
+import StockTable from '../components/StockTable';
 
 
 export default function Home() {
@@ -11,11 +11,11 @@ export default function Home() {
     <>
       <Head>
         <title>Pulgas a bordo</title>
-        <meta name="description" content="Estoque do bar" />
+        <meta name="description" content="Estoque do bar" /> 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
       <div className="App">
       <img src="https://www.example.com/random-image.jpg" alt="Random" />
       <Button type="primary" style={{ margin: '16px' }}>
@@ -24,7 +24,7 @@ export default function Home() {
       <Button type="default" style={{ margin: '16px' }}>
         Reduce Stock
       </Button>
-      <table> </table>
+      <StockTable />
       </div>
       </main>
     </>
